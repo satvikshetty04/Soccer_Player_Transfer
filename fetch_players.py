d1c2066ff7cb78c549e831138c9a824134cb60d4
Varun Jayathirtha <vjayath@ncsu.edu>
2016-11-25 18:52:39 -0500

    added git fetch script

--- fetch_players.py
+++ fetch_players.py
@@ -9,6 +9,10 @@ cur = conn.cursor()
 teams = []
 players = {}
 
+#   1 : attack
+#   2 : defense
+#   3 : midfield
+
 player_skills = {
     1: 'finishing, dribbling, volleys, heading_accuracy, curve, acceleration, sprint_speed, free_kick_accuracy, ball_control, short_passing, penalties, shot_power, balance, jumping',
     2: 'vision, ball_control, short_passing, long_passing, long_shots, marking, penalties, potential, aggression, sliding_tackle, standing_tackle',
@@ -40,27 +44,63 @@ def get_player_stats(player_id, field_pos):
 
     return player_stat/skill_count[field_pos]
 
+def get_weak_player(team):
+    result = cur.execute("SELECT player_api_id, team_api_id from PlayerTeamMod where team_api_id = " + str(team) + " AND season = '2013/2014' order by team_api_id;")
+    player_list = {}
+    for row in result:
+        player_list[row[0]] = []
+
+    team_attack = 0
+    team_defense = 0
+    team_midfield = 0
+    team_attack_count = 0
+    team_defense_count = 0
+    team_midfield_count = 0
+
+    for player_id in player_list:
+        field_pos = get_field_position(player_id)
+        player_stat = get_player_stats(player_id, field_pos)
+        player_list[player_id] = [field_pos, player_stat]
+
+        if field_pos == 1:                               # attack
+            team_attack += player_stat
+            team_attack_count += 1
+        elif field_pos == 2:                             # defense
+            team_defense += player_stat
+            team_defense_count += 1
+        else:                                            # midfield
+            team_midfield += player_stat
+            team_midfield_count += 1
+
+    team_attack /= team_attack_count
+    team_defense /= team_defense_count
+    team_midfield /= team_midfield_count
+
+    if team_attack > team_midfield:
+        min_field = 3
+        min = team_midfield
+    else:
+        min_field = 1
+        min = team_attack
+
+    if team_defense < min:
+        min_field = 2
+
+    min_stat = 100
+    for player_id in player_list:
+        if player_list[player_id][0] == min_field and min_stat > player_list[player_id][1]:
+            min_stat = player_list[player_id][1]
+            min_player = player_id
+
+    print (str(team) + " : " +str(min_player))
+    return (min_player, player_list[min_player][0])
+
 def main():
     result = cur.execute("SELECT DISTINCT team_api_id from PlayerTeamMod;")
-    count = 0
     for team in result:
-        count += 1
-        if count > 10:
-            break
         teams.append(team[0])
-        players[team[0]] = {}
 
     for team in teams:
-        result = cur.execute("SELECT player_api_id, team_api_id from PlayerTeamMod where team_api_id = " + str(team) + " AND season = '2013/2014' order by team_api_id;")
-        player_list = []
-        for row in result:
-            player_list.append(row[0])
-
-        for player_id in player_list:
-            field_pos = get_field_position(player_id)
-            player_stat = get_player_stats(player_id, field_pos)
-            players[team][player_id] = [field_pos, player_stat]
-
-    pprint(players)
+        get_weak_player(team)
 
 main()
