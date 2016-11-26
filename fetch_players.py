60abc53cac4986f77b15686ac0adafc5b5da5d05
Aditya Bhardwaj <abhardw2@ncsu.edu>
2016-11-26 18:49:23 -0500

    Added playerPredict to classify them in categories.

--- fetch_players.py
+++ fetch_players.py
@@ -2,6 +2,28 @@ import sqlite3
 import pandas as pd
 import sys
 from pprint import pprint
+import soccerAnalysis1
+import sqlite3
+import pandas as pd
+from sklearn.manifold import TSNE
+from scipy.spatial import distance
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+from sklearn.cluster import DBSCAN
+from sklearn import metrics
+from sklearn.datasets.samples_generator import make_blobs
+from sklearn.preprocessing import StandardScaler
+from bokeh.plotting import figure, ColumnDataSource, show
+from bokeh.models import HoverTool
+import numpy as np
+from pylab import plot,show
+from numpy import vstack,array
+from numpy.random import rand
+from scipy.cluster.vq import kmeans,vq
+from mpl_toolkits.mplot3d import Axes3D
+from bokeh.io import output_notebook
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 database = './data/database.sqlite'
 conn = sqlite3.connect(database)
@@ -14,24 +36,34 @@ players = {}
 #   3 : midfield
 
 player_skills = {
-    1: 'finishing, dribbling, volleys, heading_accuracy, curve, acceleration, sprint_speed, free_kick_accuracy, ball_control, short_passing, penalties, shot_power, balance, jumping',
+    0: 'finishing, dribbling, volleys, heading_accuracy, curve, acceleration, sprint_speed, free_kick_accuracy, ball_control, short_passing, penalties, shot_power, balance, jumping',
     2: 'vision, ball_control, short_passing, long_passing, long_shots, marking, penalties, potential, aggression, sliding_tackle, standing_tackle',
     3: 'marking, standing_tackle, sliding_tackle, short_passing, long_passing, crossing, jumping, stamina, strength, long_shots, aggression, interceptions, positioning'
 }
 
 skill_count = {
-    1: 14,
+    0: 14,
     2: 11,
     3: 13
 }
 
 def get_field_position(player_id):
     if player_id % 3 == 0:
-        return 1
+        return 0
     elif player_id % 3 == 1:
         return 2
     else:
         return 3
+
+def getYoungerPlayer(team_id, season):
+    cur.execute("SELECT DISTINCT PT.player_api_id, PT.team_api_id from PlayerTeamMod PT, Player P where PT.team_api_id = " + str(team_id) + " AND PT.season = " + str("'" + season + "'") +" AND " + "(SELECT (strftime('%Y', 'now') - strftime('%Y', birthday)) - (strftime('%m-%d', 'now') < strftime('%m-%d', birthday)) from Player where player_api_id = PT.player_api_id) < 21" + " order by PT.team_api_id;")
+    allrows = cur.fetchall()
+    print(allrows)
+
+def getOldestPlayer(team_id, season):
+    cur.execute("SELECT DISTINCT PT.player_api_id, PT.team_api_id from PlayerTeamMod PT, Player P where PT.team_api_id = " + str(team_id) + " AND PT.season = " + str("'" + season + "'") +" AND " + "(SELECT (strftime('%Y', 'now') - strftime('%Y', birthday)) - (strftime('%m-%d', 'now') < strftime('%m-%d', birthday)) from Player where player_api_id = PT.player_api_id) > 34" + " order by PT.team_api_id;")
+    allrows = cur.fetchall()
+    print(allrows)
 
 def get_player_stats(player_id, field_pos):
     result = cur.execute( "Select " + player_skills[field_pos] + " from Player_Attributes where player_api_id =" + str(
